cfa6f16a09a0fcb934176c134671eb92532cd780
Anuj Patel <patel.anuj1997@gmail.com>
2026-09-15 20:05:10 +0530

    Document what archived() returns

--- archive.js
+++ archive.js
@@ -1,5 +1,8 @@
 import { defaultTasks } from './tasks.js';
 
+/**
+ * @returns {Array} The subset of tasks whose `done` property is true.
+ */
 export function archived(tasks) {
   return tasks.filter((t) => t.done);
 }
